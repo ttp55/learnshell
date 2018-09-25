7c20abb852c91a6102031cc5ee7fccbc3e77d6c6
ttp55 <1047697347@qq.com>
2018-09-25 02:34:57 -0400

    输出三角形

---
+++ sanjiaoxing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "enter a number:"
+read i
+
+for ((l=0;l<${i};l++))
+do
+	for((j=${l}+1;j<${i};j++))
+	do
+	echo -e " \c"
+	done
+	for ((m=0;m<=${l};m++))
+	do
+	echo -e " \c" 
+	echo -e "*\c"
+	done
+	echo -e ""
+done
